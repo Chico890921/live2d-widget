0e2d9b12903aa61b43424a9c8a1bf0b772bd09ba
涼涼 <191147343satan@gmail.com>
2021-06-20 02:53:03 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -21,7 +21,6 @@ function loadWidget(config) {
 			<div id="waifu-tips"></div>
 			<canvas id="live2d" width="800" height="800"></canvas>
 			<div id="waifu-tool">
-				<span class="fa fa-lg fa-comment"></span>
 				<span class="fa fa-lg fa-info-circle"></span>
 				<span class="fa fa-lg fa-times"></span>
 			</div>
